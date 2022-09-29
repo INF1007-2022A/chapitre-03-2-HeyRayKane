950d1c94ebab3c9f51593a13e0c5e748ad3523e3
HeyRayKane <rayan.ajakane@polymtl.ca>
2022-09-28 23:53:30 -0400

    complétion partielle de l'exercice 3.2

--- exercice.py
+++ exercice.py
@@ -3,30 +3,45 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	puissance = (voltage ** 2)/resistance
+	return puissance
 
-def orthogonal(v1, v2):
+def orthogonal(v1, v2): #v1 et v2 sont des vecteurs
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	#v1[0] Pour accéder au X.
+	#v1[1] Pour accéder au Y.
+	produit_scalaire = (v1[0]*v2[0])+(v1[1]*v2[1])
+	return (produit_scalaire == 0) #Retourne vrai ou faux
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	nombre_element = 0
+	somme=0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v>=0:
+			nombre_element+=1
+			somme+=v
+	moyenne = somme/nombre_element
+	return moyenne # La variable v contient une valeur de la liste.
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties, tens, fives, twos, ones = 0,0,0,0,0
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties = value//20
+			value -= 20*twenties #on aurait pu utiliser aussi l'opérateur %
 		elif value >= 10:
-			pass
+			tens = value//10
+			value -= 10*tens
 		elif value >= 5:
-			pass
+			fives = value//5
+			value -= 5*fives
+		elif value >= 2:
+			twos = value//2
+			value -= 2*twos
 		elif value >= 1:
-			pass
+			ones = value
 
 	return (twenties, tens, fives, twos, ones);
 
